79351d93ce3825ae7651c6e6ea1b6cc3853fe3dd
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-26 21:42:12 -0500

    create and read from database working

--- newsapp/app.js
+++ newsapp/app.js
@@ -4,19 +4,55 @@ const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
 const pgp = require("pg-promise")();
 const bcrypt = require("bcrypt");
+const session = require("express-session");
+const path = require("path");
 const CONNECTION_STRING = "postgres://postgres:Americ@@localhost:5432/newsdb";
 const PORT = 3000;
 const SALT_ROUNDS = 10;
 
-app.engine("mustache", mustacheExpress());
-app.set("views", "./views");
+const VIEWS_PATH = path.join(__dirname, "/views");
+
+app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"));
+app.set("views", VIEWS_PATH);
 app.set("view engine", "mustache");
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "abcdefg",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 const db = pgp(CONNECTION_STRING);
 
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.get("/users/add-article", (req, res) => {
+  res.render("add-article");
+});
+
+app.post("/users/add-article", (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let userId = req.session.user.userId;
+
+  db.none("INSERT INTO articles(title,body,userid) VALUES ($1, $2, $3)", [
+    title,
+    description,
+    userId,
+  ]).then(() => res.send("SUCCESS!"));
+});
+
+app.get("/users/articles", (req, res) => {
+  let userId = req.session.user.userId;
+  db.any("SELECT articleid,title,body FROM articles WHERE userid = $1", [
+    userId,
+  ]).then((articles) => {
+    res.render("articles", { articles: articles });
+  });
 });
 
 app.post("/login", (req, res) => {
@@ -29,7 +65,10 @@ app.post("/login", (req, res) => {
     if (user) {
       bcrypt.compare(password, user.password, function (error, result) {
         if (result) {
-          res.send("SUCCESS!");
+          if (req.session) {
+            req.session.user = { userId: user.userid, username: user.username };
+          }
+          res.redirect("/users/articles");
         } else {
           res.render("login", { message: "Invalid username or password" });
         }
