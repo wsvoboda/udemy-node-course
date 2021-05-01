eddde203d26cd0400d5c4093cd00bbdeeab8b196
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-30 22:32:30 -0500

    creates model using sequelize

---
+++ install-sequelize/models/dish.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Dish extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Dish.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.DOUBLE
+  }, {
+    sequelize,
+    modelName: 'Dish',
+  });
+  return Dish;
+};
