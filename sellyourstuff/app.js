fd5e2de973f3d5420722f1d7584dfefdca306382
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-01 17:15:23 -0500

    registration with error working

--- sellyourstuff/app.js
+++ sellyourstuff/app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const mustacheExpress = require("mustache-express");
 const path = require("path");
+const models = require("./models");
+const bcrypt = require("bcrypt");
 
+const SALT_ROUNDS = 10;
 const PORT = 3000;
 const VIEWS_PATH = path.join(__dirname, "/views");
+
+app.use(express.urlencoded({ extended: false }));
 
 app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"));
 app.set("views", VIEWS_PATH);
@@ -12,6 +17,35 @@ app.set("view engine", "mustache");
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  let persistedUser = await models.User.findOne({
+    where: { username: username },
+  });
+  if (persistedUser == null) {
+    bcrypt.hash(password, SALT_ROUNDS, async (error, hash) => {
+      if (error) {
+        res.render("register", { message: "Error creating user!" });
+      } else {
+        let user = models.User.build({
+          username: username,
+          password: hash,
+        });
+        let savedUser = await user.save();
+        if (savedUser != null) {
+          res.redirect("/login");
+        } else {
+          res.render("register", { message: "User already exists!" });
+        }
+      }
+    });
+  } else {
+    res.render("register", { message: "User already exists!" });
+  }
 });
 
 app.get("/login", (req, res) => {
