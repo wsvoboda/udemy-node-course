9ca42d323400266eb9ea2225c6e8ac9a44d85be9
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-02 12:42:55 -0500

    upload and display function working

--- sellyourstuff/routes/users.js
+++ sellyourstuff/routes/users.js
@@ -2,11 +2,21 @@ const express = require("express");
 const formidable = require("formidable");
 const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
+const models = require("../models");
 
 let uniqueFileName = "";
 
 router.get("/add-product", (req, res) => {
   res.render("users/add-product");
+});
+
+router.get("/products", async (req, res) => {
+  let products = await models.Product.findAll({
+    where: {
+      userId: req.session.user.userId,
+    },
+  });
+  res.render("users/products", { products: products });
 });
 
 function uploadFile(req, callback) {
@@ -21,6 +31,27 @@ function uploadFile(req, callback) {
       callback(file.name);
     });
 }
+
+router.post("/add-product", async (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let price = parseFloat(req.body.price);
+  let userId = req.session.user.userId;
+
+  let product = models.Product.build({
+    title: title,
+    description: description,
+    price: price,
+    userId: userId,
+    imageURL: uniqueFileName,
+  });
+  let persistedProduct = await product.save();
+  if (persistedProduct != null) {
+    res.redirect("/users/products");
+  } else {
+    res.render("users/add-product", { message: "Unable to add product" });
+  }
+});
 
 router.post("/upload", (req, res) => {
   uploadFile(req, (photoURL) => {
